5b9af4424605136ad17f3e3936c0caec1d1c66d4
unknown <beamb366@gmail.com>
2023-03-09 19:23:51 +0700

    first commit

---
+++ LoginForm.js
@@ -0,0 +1,10 @@
+function register(){
+    document.getElementById("LoginForm").style.transform = "translatex(0px)";
+    document.getElementById("RegForm").style.transform = "translatex(0px)";
+    document.getElementById("Indiacator").style.transform = "translatex(100px)";
+}
+function login() {
+    document.getElementById("RegForm").style.transform = "translatex(300px)";
+    document.getElementById("LoginForm").style.transform = "translatex(300px)";
+    document.getElementById("Indicator").style.transform = "translatex(0px)";
+}
